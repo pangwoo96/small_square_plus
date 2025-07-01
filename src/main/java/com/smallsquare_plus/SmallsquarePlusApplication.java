fbe65281d45461e5ed907b1ce615412cce68302e
pangwoo96 <junggw96@gmail.com>
2025-07-01 20:12:02 +0900

    first commit

---
+++ src/main/java/com/smallsquare_plus/SmallsquarePlusApplication.java
@@ -0,0 +1,13 @@
+package com.smallsquare_plus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallsquarePlusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmallsquarePlusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/smallsquare_plus/SmallsquarePlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smallsquare_plus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmallsquarePlusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
