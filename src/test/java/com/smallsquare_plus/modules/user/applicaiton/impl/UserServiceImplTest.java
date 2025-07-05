4927307b58e5d17bec98d4edaac70b514b24bf8e
pangwoo96 <junggw96@gmail.com>
2025-07-05 17:27:22 +0900

    test: 로그아웃 기능 구현

--- src/test/java/com/smallsquare_plus/modules/user/applicaiton/impl/UserServiceImplTest.java
+++ src/test/java/com/smallsquare_plus/modules/user/applicaiton/impl/UserServiceImplTest.java
@@ -3,9 +3,12 @@ package com.smallsquare_plus.modules.user.applicaiton.impl;
 import com.smallsquare_plus.modules.user.application.impl.UserServiceImpl;
 import com.smallsquare_plus.modules.user.domain.entity.User;
 import com.smallsquare_plus.modules.user.infrastructure.jwt.JwtProvider;
+import com.smallsquare_plus.modules.user.infrastructure.jwt.JwtUtil;
 import com.smallsquare_plus.modules.user.infrastructure.mapper.UserMapper;
+import com.smallsquare_plus.modules.user.infrastructure.redis.RedisService;
 import com.smallsquare_plus.modules.user.utils.UserUtils;
 import com.smallsquare_plus.modules.user.web.dto.request.UserLoginReqDTO;
+import com.smallsquare_plus.modules.user.web.dto.request.UserLogoutReqDTO;
 import com.smallsquare_plus.modules.user.web.dto.request.UserSignupReqDTO;
 import com.smallsquare_plus.modules.user.web.dto.response.UserLoginResDTO;
 import org.junit.jupiter.api.MethodOrderer;
@@ -41,6 +44,12 @@ public class UserServiceImplTest {
 
     @Mock
     private JwtProvider jwtProvider;
+
+    @Mock
+    private JwtUtil jwtUtil;
+
+    @Mock
+    private RedisService redisService;
 
     private UserSignupReqDTO createUserSignupReqDto() {
         return UserSignupReqDTO.builder()
@@ -98,5 +107,27 @@ public class UserServiceImplTest {
         assertThat(resDTO.getRefreshToken()).isEqualTo("testRefreshToken");
     }
 
+    @Test
+    @Order(3)
+    void 로그아웃() {
+
+        //given
+        UserLogoutReqDTO reqDTO = UserLogoutReqDTO.builder()
+                .accessToken("testAccessToken")
+                .refreshToken("testRefreshToken")
+                .build();
+
+        given(jwtUtil.getRemainingExpirationMillis(reqDTO.getAccessToken())).willReturn(100L);
+        given(jwtUtil.getRemainingExpirationMillis(reqDTO.getRefreshToken())).willReturn(100L);
+
+        //when
+        userService.logout(reqDTO);
+
+        //then
+        verify(jwtUtil).getRemainingExpirationMillis("testAccessToken");
+        verify(jwtUtil).getRemainingExpirationMillis("testRefreshToken");
+        verify(redisService).saveBlacklist("testAccessToken", 100L, "testRefreshToken", 100L);
+
+    }
 
 }
