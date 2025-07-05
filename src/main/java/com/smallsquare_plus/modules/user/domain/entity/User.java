60d9f99e76c43760513fdb460b6d4af69d7f261b
pangwoo96 <junggw96@gmail.com>
2025-07-05 15:00:14 +0900

    chore: User 엔티티 설정

--- src/main/java/com/smallsquare_plus/modules/user/domain/entity/User.java
+++ src/main/java/com/smallsquare_plus/modules/user/domain/entity/User.java
@@ -1,5 +1,24 @@
 package com.smallsquare_plus.modules.user.domain.entity;
 
+import com.smallsquare_plus.modules.user.domain.enums.IsActive;
+import com.smallsquare_plus.modules.user.domain.enums.Role;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
 public class User {
 
+    private Long userId;
+    private String username;
+    private String password;
+    private String nickname;
+    private String email;
+    private String name;
+    private IsActive isActive;
+    private Role role;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
