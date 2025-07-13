6422dc5f2e12f080413bfbfe97f8d4e39405bad2
pangwoo96 <156070047+pangwoo96@users.noreply.github.com>
2025-07-13 20:56:42 +0900

    Feat/view signup (#11)
    * feat: vue 테스트
    
    * feat: vue 페이지 테스트

---
+++ frontend/src/pages/Home.vue
@@ -0,0 +1,27 @@
+<!-- src/views/Home.vue -->
+
+<template>
+  <div>
+    <h1>홈 화면입니다.</h1>
+    <p>이 페이지는 메인 페이지입니다.</p>
+    <button @click="goToLogin">로그인 하러 가기</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useRouter } from "vue-router";
+const router = useRouter();
+const goToLogin = () => {
+  router.push("/login"); // url
+}
+</script>
+
+<style scoped>
+h1 {
+  color: teal;
+}
+button {
+  margin-top: 20px;
+  padding: 10px;
+}
+</style>
